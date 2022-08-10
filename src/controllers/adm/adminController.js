8dcd4c8ca2a56caa2150e2d87c470885c7b18b6d
RondinelliNelvam <rondi135@gmail.com>
2022-08-10 08:14:45 -0300

    Criação do metodo Create do Admin

--- src/controllers/adm/adminController.js
+++ src/controllers/adm/adminController.js
@@ -1,1 +1,14 @@
-const database = require('../models')
+const database = require('../../db/models')
+
+class AdminController {
+  static async createSystem(req, res) {
+    const system = req.body
+    try {
+      const newsystem = await database.Systems.create(system)
+      return res.status(200).json(system)
+    } catch (error) {}
+    return res.status(500).json(error.message)
+  }
+}
+
+module.exports = AdminController
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,5 +1,9 @@
 const { Router } = require('express')
-
+const AdminController = require('../controllers/adm/adminController')
 const router = Router()
+
+module.exports = router
+
+router.post('/system', AdminController.createSystem)
 
 module.exports = router

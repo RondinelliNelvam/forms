43fe0592e24736f164ff7caeb8dadad30dab2830
RondinelliNelvam <rondi135@gmail.com>
2022-08-10 07:21:44 -0300

    Rota admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser')
 const userRouter = require('./userRoute')
+const adminRouter = require('./adminRouter')
 
 module.exports = (route) => {
-  route.use(bodyParser.json(), userRouter)
+  route.use(bodyParser.json(), userRouter, adminRouter)
 }
